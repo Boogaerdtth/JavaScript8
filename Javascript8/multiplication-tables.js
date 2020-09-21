be20e0c06e2b8cf96668eaff3dfdc9f2f6a95d3e
Boogaerdtth <70010486+Boogaerdtth@users.noreply.github.com>
2020-09-21 14:18:26 +0200

    Add files via upload

---
+++ Javascript8/multiplication-tables.js
@@ -0,0 +1,12 @@
+for (i = 0; i <= 10; i++) {
+    let x = i * 9;
+    console.log(x);
+}
+
+for (let x = 0; x <= 10; x++) {
+    for (let i = 1; i <= 10; i++) {
+        let result = x * i;
+        console.log(result);
+    }
+}
+
